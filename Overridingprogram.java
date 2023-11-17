1b5da318726a7c607d5b85870d8c8e315a263d38
seleniumcoding9 <selenium197@gmail.com>
2023-11-17 05:33:43 -0800

    overridding

---
+++ Overridingprogram.java
@@ -0,0 +1,31 @@
+public class Overridingprogram
+{
+    public static void main(String[] args)
+{
+    child C = new child();
+
+    int result1 = C.m1();
+
+    System.out.println(result1);
+
+    int result2 = C.m2();
+
+    System.out.println(result2);
+
+    int result3 = C.m3();
+
+    System.out.println(result3);
+
+    int result4 = C.m4();
+
+    System.out.println(result4);
+
+    int result5 = C.m5();
+
+    System.out.println(result5);
+
+    int result6 = C.m6();
+
+    System.out.println(result6);
+    }
+}
